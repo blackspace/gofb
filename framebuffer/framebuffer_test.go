b755046de0455ed6e1fd7ef9a1df7896e140e817
weiwufeng <weiwufeng@sohu.com>
2016-10-11 23:05:00 +0800

    ...

--- framebuffer/framebuffer_test.go
+++ framebuffer/framebuffer_test.go
@@ -3,13 +3,15 @@ package framebuffer
 import (
 	"testing"
 	"github.com/ungerik/go-cairo"
+	"image"
 )
 
 func BenchmarkFill(b *testing.B) {
 	fb:=NewFramebuffer()
-	defer fb.Release()
+
 
 	fb.Init()
+	defer fb.Release()
 
 	for i:=0;i<b.N;i++ {
 		fb.Fill(0, 0, 255, 0)
@@ -18,7 +20,7 @@ func BenchmarkFill(b *testing.B) {
 
 }
 
-func BenchmarkMakeImage(b *testing.B) {
+func BenchmarkMakeSurfaceImage(b *testing.B) {
 	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, 1680, 1050)
 	defer surface.Finish()
 	for i:=0;i<b.N;i++ {
@@ -37,22 +39,11 @@ func BenchmarkMakeImage(b *testing.B) {
 
 func BenchmarkDrawImage(b *testing.B) {
 	fb:=NewFramebuffer()
-	defer fb.Release()
+
 	fb.Init()
+	defer fb.Release()
 
-	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, fb.Xres, fb.Yres)
-	defer surface.Finish()
-
-	surface.SetSourceRGBA(1,1,1,1)
-	surface.Rectangle(0,0,240,80)
-	surface.Fill()
-	surface.SelectFontFace("serif", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-	surface.SetFontSize(32.0)
-	surface.SetSourceRGBA(0,0,0,.1)
-	surface.MoveTo(10.0, 50.0)
-	surface.ShowText("Hello World")
-	image:=surface.GetImage()
-
+	image:=image.NewRGBA(image.Rect(0,0,fb.Xres,fb.Yres))
 
 	for i:=0;i<b.N;i++ {
 		fb.DrawImage(0,0,image)
@@ -63,8 +54,9 @@ func BenchmarkDrawImage(b *testing.B) {
 
 func BenchmarkCairoGetData(b *testing.B) {
 	fb:=NewFramebuffer()
-	defer fb.Release()
+
 	fb.Init()
+	defer fb.Release()
 
 	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, fb.Xres, fb.Yres)
 	defer surface.Finish()
@@ -77,24 +69,53 @@ func BenchmarkCairoGetData(b *testing.B) {
 
 func BenchmarkDrawData(b *testing.B) {
 	fb:=NewFramebuffer()
-	defer fb.Release()
+
 	fb.Init()
+	defer fb.Release()
 
-	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, fb.Xres, fb.Yres)
-	defer surface.Finish()
+	data:=make([]byte,fb.Xres*fb.Yres*4,fb.Xres*fb.Yres*4)
 
-	surface.Flush()
+	for i:=0;i<b.N;i++ {
+		fb.DrawData(0,0,data,fb.Xres,fb.Yres)
+	}
+}
+
+func BenchmarkCopyDataByLine(b *testing.B) {
+	fb:=NewFramebuffer()
+
+	fb.Init()
+	defer fb.Release()
+
+	data1:=make([]byte,fb.Xres*fb.Yres*4,fb.Xres*fb.Yres*4)
+	data2:=make([]byte,fb.Xres*fb.Yres*4,fb.Xres*fb.Yres*4)
 
 
 	for i:=0;i<b.N;i++ {
-		fb.DrawData(0,0,surface.GetData(),fb.Xres,fb.Yres)
+		for y:=0;y<fb.Yres;y++ {
+			copy(data1[y*fb.Xres:(y+1)*fb.Xres],data2[y*fb.Xres:(y+1)*fb.Xres])
+		}
+	}
+}
+
+func BenchmarkCopyDataAllInOne(b *testing.B) {
+	fb:=NewFramebuffer()
+
+	fb.Init()
+	defer fb.Release()
+
+	data1:=make([]byte,fb.Xres*fb.Yres*4,fb.Xres*fb.Yres*4)
+	data2:=make([]byte,fb.Xres*fb.Yres*4,fb.Xres*fb.Yres*4)
+
+	for i:=0;i<b.N;i++ {
+		copy(data1, data2)
 	}
 }
 
 func BenchmarkAnimation(b *testing.B) {
 	fb:=NewFramebuffer()
-	defer fb.Release()
+
 	fb.Init()
+	defer fb.Release()
 
 	surface := cairo.NewSurface(cairo.FORMAT_ARGB32, fb.Xres, fb.Yres)
 	defer surface.Finish()
